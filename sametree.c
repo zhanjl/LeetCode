524af2a2ae72e4574e6b9744c04993d0733b5a9d
zhanjl <zhanjl@zhanjl-HP-Compaq-8080-Elite-CMT-PC.(none)>
2014-10-07 17:35:50 +0800

    update samtree.c MaxDepthofBt.c

--- sametree.c
+++ sametree.c
@@ -6,23 +6,9 @@ struct TreeNode {
 
 bool isSameTree(struct TreeNode *p, struct TreeNode *q)
 {
-    bool lchild, rchild;
-    
-    if ((p == NULL) && (q == NULL))
-        return true;
-    else if ((p != NULL) && (q != NULL))
-    {
-        if (p->val != q->val)
-            return false;
-        lchild = isSameTree(p->lchild, q->lchild);
-        if (lchild == false)
-            return false;
-        rchild = isSameTree(p->rchild, q->rchild);
-        if (rchild == false)
-            return false;
-        return true;
-    }
-    else
-        return false;
+    if ((p == NULL) || (q == NULL))
+        return p == q;
+    return (p->val == q->val) && isSameTree(p->lchild, q->lchild) &&
+                                isSameTree(p->rchild, q->rchild);
 }
 
